275e334148ef354da2f86f3a17638367e8ddef1f
ghadiyakrushil <ghadiyakrushil@gmail.com>
2025-04-27 15:02:21 +0530

    image changes

--- src/app/data/category.ts
+++ src/app/data/category.ts
@@ -4,36 +4,42 @@ export const category = [
     categoryId: 1,
     router: 'multivitamin-supplement',
     img: 'multivitamin-supplement.jpg',
+    url: 'https://ik.imagekit.io/qbulnpi06/banner/category-img/multivitamin-supplement.jpg?updatedAt=1745745327462'
   },
   {
     category: 'JOINT SUPPLEMENT',
     categoryId: 2,
     router: 'joint-supplement',
     img: 'joint-supplement.jpg',
+    url: 'https://ik.imagekit.io/qbulnpi06/banner/category-img/joint-supplement.jpg?updatedAt=1745745323477'
   },
   {
     category: 'WEIGHT LOSS SUPPLEMENT',
     categoryId: 3,
     router: 'weight-loss-supplement',
     img: 'weight-loss-supplement.jpg',
+    url: 'https://ik.imagekit.io/qbulnpi06/banner/category-img/weight-loss-supplement.jpg?updatedAt=1745745328075'
   },
   {
     category: 'EYE SUPPLEMENT',
     categoryId: 4,
     router: 'eye-supplement',
     img: 'eye-supplement.jpg',
+    url: 'https://ik.imagekit.io/qbulnpi06/banner/category-img/eye-supplement.jpg?updatedAt=1745745323366'
   },
   {
     category: 'IRON SUPPLEMENT',
     categoryId: 5,
     router: 'iron-supplement',
     img: 'iron-supplement.jpg',
+    url: 'https://ik.imagekit.io/qbulnpi06/banner/category-img/iron-supplement.jpg?updatedAt=1745745323489'
   },
   {
     category: 'DIABETIC SUPPLEMENT',
     categoryId: 6,
     router: 'diabetic-supplement',
     img: 'diabetic-supplement.jpg',
+    url: 'https://ik.imagekit.io/qbulnpi06/banner/category-img/diabetic-supplement.jpg?updatedAt=1745745323372'
   },
   {
     category: 'HNS SUPPLEMENT',
@@ -46,30 +52,35 @@ export const category = [
     categoryId: 8,
     router: 'digestive-supplement',
     img: 'digestive-supplement.jpg',
+    url: 'https://ik.imagekit.io/qbulnpi06/banner/category-img/digestive-supplement.jpg?updatedAt=1745745322997'
   },
   {
     category: 'ENERGY SUPPLEMENT',
     categoryId: 9,
     router: 'energy-supplement',
     img: 'energy-supplement.jpg',
+    url: 'https://ik.imagekit.io/qbulnpi06/banner/category-img/energy-supplement.jpg?updatedAt=1745745323474'
   },
   {
     category: 'KIDS SUPPLEMENT',
     categoryId: 10,
     router: 'kids-supplement',
     img: 'kids-supplement.png',
+    url: 'https://ik.imagekit.io/qbulnpi06/banner/category-img/kids-supplement.png?updatedAt=1745745327638'
   },
   {
     category: 'ANTI-CANCER SUPPLEMENT',
     categoryId: 11,
     router: 'anti-cancer-supplement',
     img: 'anti-cancer-supplement.jpg',
+    url: 'https://ik.imagekit.io/qbulnpi06/banner/category-img/anti-cancer-supplement.jpg?updatedAt=1745745323370'
   },
   {
     category: 'IMMUNITY SUPPLEMENTS',
     categoryId: 12,
     router: 'immunity-supplement',
     img: 'immunity-supplement.jpg',
+    url: 'https://ik.imagekit.io/qbulnpi06/banner/category-img/immunity-supplement.jpg?updatedAt=1745745323057'
   },
   {
     category: 'ACTIVE PHARMACEUTICAL INGREDIENT',
@@ -82,5 +93,6 @@ export const category = [
     categoryId: 14,
     router: 'phosphodiesterase',
     img: 'phosphodiesterase.jpeg',
+    url: 'https://ik.imagekit.io/qbulnpi06/banner/category-img/phosphodiesterase.jpeg?updatedAt=1745745327939'
   },
 ];
